a9b08209c1b5b0d6f715145fc6535ec377e7c0b9
sobadon <sbdn@chasoba.net>
2021-12-10 14:29:06 +0900

    fms2 への接続がタイムアウトしたときでも録画を継続するように

--- cmd/agqr-toshitai-recording/rec-backup/root.go
+++ cmd/agqr-toshitai-recording/rec-backup/root.go
@@ -1,6 +1,7 @@
 package recbackup
 
 import (
+	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -57,12 +58,16 @@ func rec() error {
 	durationSec := 60 * 50
 	err = agqr.Rec(durationSec, workingAbsPath)
 	if err != nil {
-		return errors.WithStack(err)
+		// TODO: とりあえずの処置なので後でどうにかする
+		// ffmpeg が "Connection to tcp://fms2.uniqueradio.jp:443 failed: Operation timed out" で終了したときでも録画を継続させる
+		log.Printf("%+v\n", err)
 	}
 
 	err = os.Rename(workingAbsPath, archiveAbsPath)
 	if err != nil {
-		return errors.WithStack(err)
+		// TODO: とりあえずの処置なので後でどうにかする
+		// ffmpeg がコケてファイルが生成されていないときでも録画を継続させる
+		log.Printf("%+v\n", err)
 	}
 
 	return nil
